b503780f9cf96a5a6bcb88d13fa3402b0630064b
Gurdeep Singh <gurdeep.seoily@gmail.com>
2025-11-10 12:23:55 +0530

    Updated code

---
+++ Hello_World.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  int n = int.parse(stdin.readLineSync()!);
+  print(n);
+  // for(int i=0,)
+
+  // int age = 20;
+  // double price = 59.99;
+  // String name = "Gurdeep";
+  // bool isActive = true;
+
+  // print(price);
+  // print(name);
+  // print(isActive);
+  // print("Hello World");
+
+  // print("My name is $name and my age is ($age+2)");
+
+  // List<int> numbers = [10, 20, 30, 40];
+
+  // numbers.forEach((num) {
+  //   print(num);
+  // });
+
+  // List<int> digits = [100, 200, 300, 400];
+  // for (var me in digits) {
+  //   print(me);
+  // }
+}
